7c98e4509e62707d6af1461b2d3c9a388518c436
James T <jtara@tuta.io>
2017-03-01 15:09:40 -0800

    basic templates and webforms working

---
+++ config.py
@@ -0,0 +1,7 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'admin'
+
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},]
