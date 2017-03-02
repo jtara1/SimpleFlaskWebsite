4bedbad16488f73c7abde8c848f3581e406dd00d
James T <jtara@tuta.io>
2017-03-01 18:39:02 -0800

    setup database and user can now login using yahoo openid

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'admin'
 
@@ -5,3 +8,9 @@ OPENID_PROVIDERS = [
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
